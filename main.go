5921fea3a60f6b679d869f9eafc3e5c98f481750
= <=>
2024-03-26 22:13:55 -0300

    Criar instâncias struct ContaCorrente
    Alterações realizadas conforme o vídeo "Utilizando a struct"

--- main.go
+++ main.go
@@ -10,5 +10,16 @@ type ContaCorrente struct {
 }
 
 func main() {
-	fmt.Println(ContaCorrente{})
+	// var contaDoGuilherme ContaCorrente = ContaCorrente{}
+	contaDoGuilherme := ContaCorrente{
+		titular:       "Guilherme",
+		numeroAgencia: 589,
+		numeroConta:   1234256,
+		saldo:         125.5,
+	}
+
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
 }
